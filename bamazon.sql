e1ea71d6df8d22b9bd9311137e43aa3fc78b7027
sakiohma <sakiohma@github.com>
2018-04-25 02:20:24 -0400

    added changes

--- bamazon.sql
+++ bamazon.sql
@@ -1,30 +1,67 @@
--- Initiating database --
-DROP DATABASE IF EXISTS bamazon_db;
-CREATE DATABASE bamazon_db;
+DROP DATABASE IF EXISTS bamazon;
+CREATE DATABASE bamazon;
 
-USE bamazon_db;
+USE bamazon;
 
--- Table including list of products for sale --
-CREATE TABLE products (
-    item_id INT NOT NULL AUTO_INCREMENT,
-	product_name VARCHAR (225) NOT NULL,
-	department_name VARCHAR (225) NOT NULL,
-	price DECIMAL (10,2) NOT NULL,
-	stock_quantity INT (11) NOT NULL,
-	PRIMARY KEY (item_id)
+CREATE TABLE products
+(
+    item_id INT
+    AUTO_INCREMENT NOT NULL,
+  product_name VARCHAR
+    (45) NOT NULL,
+  product_sales DECIMAL
+    (10,2) DEFAULT 0,
+  department_name VARCHAR
+    (45) NOT NULL,
+  price DECIMAL
+    (10,2) NOT NULL,
+  stock_quantity INT
+    (10) NOT NULL,
+  primary key
+    (item_id)
 );
 
--- Insert data into the 'products' table --
-INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ('Kraft Blue Cheese Salad Dressing', 'Grocery', 2.99, 425),
-       ('Jordache Leggings', 'Clothing', 9.99, 300),
-       ('Danskin Athletic Top', 'Clothing', 12.99, 338),
-       ('Shea Moisture Shampoo', 'Cosmetics', 8.99, 280),
-       ('Breyers Vailla Ice Cream', 'Grocery', 3.95, 565),
-       ('Maybelline Lipstick', 'Cosmetics', 2.50, 400),
-       ('Oyin Handmade Conditioner', 'Cosmetics', 11.50, 354),
-       ('Nabisco Chocolate Chip Cookies', 'Grocery', 2.75, 960),
-       ('Faded Glory Sweater', 'Clothing', 7.50, 432),
-       ('Revlon Eye Shadow', 'Cosmetics', 1.75, 185);
+    SELECT *
+    FROM products;
+
+    INSERT INTO products
+        (product_name, department_name, price, stock_quantity)
+    VALUES
+        ("Uncharted 4", "Video Games", 49.95, 150),
+        ("DOOM", "Video Games", 59.99, 200),
+        ("Crate of Spam", "Food and Drink", 24.50, 50),
+        ("Cool Shades", "Apparel", 75.00, 5),
+        ("Worn Denim Jeans", "Apparel", 54.25, 35),
+        ("Survival Towel", "Necessities", 42.42, 42),
+        ("Bill and Ted's Excellent Adventure", "Films", 15.00, 25),
+        ("Mad Max: Fury Road", "Films", 25.50, 57),
+        ("Monopoly", "Board Games", 30.50, 35),
+        ("Yahtzee", "Board Games", 19.95, 23);
+
+    CREATE TABLE departments
+    (
+        department_id INT
+        AUTO_INCREMENT NOT NULL,
+  department_name VARCHAR
+        (45) NOT NULL,
+  over_head_costs DECIMAL
+        (10,2) NOT NULL,
+  primary key
+        (department_id)
+);
+
+        SELECT *
+        FROM departments;
+
+        INSERT INTO departments
+            (department_name, over_head_costs)
+        VALUES
+            ("Video Games", 200),
+            ("Food and Drink", 100),
+            ("Apparel", 50),
+            ("Necessities", 300),
+            ("Films", 35),
+            ("Board Games", 0);
+
 
 
